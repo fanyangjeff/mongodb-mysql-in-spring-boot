29133acbe0bedd79013478e5e0689ec8524dbdbc
Fan Yang <fanyang@fans-air.myfiosgateway.com>
2021-07-25 00:13:24 -0400

    first commit

---
+++ src/test/java/com/mysqlmongo/MysqlMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysqlmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
